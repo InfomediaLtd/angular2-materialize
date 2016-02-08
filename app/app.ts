6e74a3b7f3d7a0b9c405861816a7d1d1b90b429c
Ruby Boyarski <rboyarski@infomedia.com.au>
2016-02-08 16:02:04 +0200

    fix(app): Updated test app to use routes

--- app/app.ts
+++ app/app.ts
@@ -6,23 +6,21 @@ import {Dropdown} from "./components/dropdown";
 import {Collapsible} from "./components/collapsible";
 import {Buttons} from "./components/buttons";
 import {Component} from "angular2/core"
+import {RouteConfig, ROUTER_DIRECTIVES, Location} from "angular2/router"
 import "../src/index"
 
 @Component({
     selector: "my-app",
-    directives: [Buttons, Collapsible, Dropdown, Dialogs, Tabs, Forms, SideNav],
-    styles: [`
-      header, main, footer { padding-left: 240px; }
-      @media only screen and (max-width : 992px) {
-        header, main, footer { padding-left: 0; }
-      }
-      `],
+    directives: [ROUTER_DIRECTIVES, Buttons, Collapsible, Dropdown, Dialogs, Tabs, Forms, SideNav],
+    styles: [`header, main, footer { padding-left: 260px; }`],
     template: `
 
         <sideNav></sideNav>
 
         <main>
-          <h5>Buttons</h5>
+          <br/><br/>
+          <router-outlet></router-outlet>
+          <!--<h5>Buttons</h5>
           <buttons></buttons>
           <br/>
           <h5>Collapsible</h5>
@@ -39,11 +37,19 @@ import "../src/index"
           <br/>
           <h5>Forms</h5>
           <forms></forms>
-          <br/>
+          <br/>-->
         </main>
 
     `
 })
+@RouteConfig([
+  {path: "/buttons", component: Buttons, name: "Buttons", useAsDefault:true},
+  {path: "/collapsible", component: Collapsible, name: "Collapsible"},
+  {path: "/dialogs", component: Dialogs, name: "Dialogs"},
+  {path: "/dropdowns", component: Dropdown, name: "Dropdown"},
+  {path: "/forms", component: Forms, name: "Forms"},
+  {path: "/tabs", component: Tabs, name: "Tabs"}
+])
 export class App {
 
 }
--- app/components/side-nav.ts
+++ app/components/side-nav.ts
@@ -1,16 +1,30 @@
 import {MaterializeDirective} from "../../src/index";
 import {Component} from "angular2/core"
+import {RouterLink} from "angular2/router"
 
 @Component({
     selector: "sideNav",
-    directives: [MaterializeDirective],
+    directives: [MaterializeDirective, RouterLink],
+    styles: [`
+      nav {
+          color: #fff;
+          background-color: #ee6e73;
+          width: 100%;
+          height: 56px;
+          line-height: 56px;
+      }
+    `],
     template: `
       <nav>
         <ul id="slide-out" class="side-nav fixed">
-          <li><a href="#!">First Sidebar Link</a></li>
-          <li><a href="#!">Second Sidebar Link</a></li>
+          <li><a [routerLink]="['Buttons']">Buttons</a></li>
+          <li><a [routerLink]="['Collapsible']">Collapsible</a></li>
+          <li><a [routerLink]="['Dialogs']">Dialogs</a></li>
+          <li><a [routerLink]="['Dropdown']">Dropdown</a></li>
+          <li><a [routerLink]="['Forms']">Forms</a></li>
+          <li><a [routerLink]="['Tabs']">Tabs</a></li>
         </ul>
-        <a materialize="sideNav" href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="mdi-navigation-menu"></i></a>
+        <a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="mdi-navigation-menu"></i></a>
       </nav>
     `
 })

d4ebebedb5883e28c17bf4f547aea38f96480cca
Ruby Boyarski <rboyarski@infomedia.com.au>
2016-12-27 13:57:34 +0200

    docs: Added sample code for chip selection

--- app/components/chips.ts
+++ app/components/chips.ts
@@ -6,7 +6,7 @@ import {Component} from "@angular/core"
         <div class="chip">Tag1<i class="close material-icons">close</i></div>
         <div class="chip">Tag2<i class="close material-icons">close</i></div>
         <br/><hr/>
-        <div class="chips" materialize="material_chip" (chip.add)="add($event.detail)" (chip.delete)="delete($event.detail)"></div>
+        <div class="chips" materialize="material_chip" (chip.add)="add($event.detail)" (chip.delete)="delete($event.detail)" (chip.select)="select($event.detail)"></div>
         <div class="chips chips-initial" materialize="material_chip" [materializeParams]="[chipsInit]"></div>
         <div class="chips chips-placeholder" materialize="material_chip" [materializeParams]="[chipsPlaceholder]"></div>
         <br/><hr/>
@@ -35,5 +35,9 @@ export class Chips {
 
   delete(chip) {
     console.log("Chip deleted: " + chip.tag);
+  }
+
+  select(chip) {
+    console.log("Chip selected: " + chip.tag);
   }
 }

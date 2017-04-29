2170aaf02bc9b9978b9fc1e847a17d7b465f14dc
lthompson <lthompson@infomedia.com.au>
2017-04-29 12:08:57 +1000

    refactor: Remove the modification of the name. Not required.

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ ROLLUP="node node_modules/.bin/rollup"
 $NGC -p src/tsconfig.json
 
 # Run rollup to generate dist directory
-$ROLLUP build/index.js -o dist/index.js -f umd -n materialize-module -u angular2-materialize
+$ROLLUP build/index.js -o dist/index.js -f umd -n angular2-materialize -u angular2-materialize
 
 # Copy all files from build to dist, except for JavaScript files
 rsync -a build/ dist

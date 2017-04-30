b798a44c445f1bbcfddc08089405807ea580f1c1
Ruby Boyarski <rboyarski@infomedia.com.au>
2017-04-30 11:15:16 +0300

    fix: Fixed release versioning

--- build.sh
+++ build.sh
@@ -6,21 +6,21 @@ rm -rf dist
 
 # Define commands
 NGC="node node_modules/.bin/ngc"
-ROLLUP="node node_modules/.bin/rollup"
+# ROLLUP="node node_modules/.bin/rollup"
 
 # Run Angular Compiler to generate build directory
 $NGC -p src/tsconfig.json
 
 # Run rollup to generate dist directory
-$ROLLUP build/index.js -o dist/index.js -f umd -n angular2-materialize -u angular2-materialize
+# $ROLLUP build/index.js -o dist/index.js -f umd -n angular2-materialize -u angular2-materialize
 
 # Copy all files from build to dist, except for JavaScript files
 rsync -a build/ dist
 
 # Copy package.json and other resources to dist
-cp README.md dist
-cp LICENSE dist
-cp .npmignore dist
+# cp README.md dist
+# cp LICENSE dist
+# cp .npmignore dist
 
 # Clean up build directory
 rm -rf build

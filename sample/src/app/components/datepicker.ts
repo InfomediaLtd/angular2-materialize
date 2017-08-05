c1b9c2be60d41b44de0ad503ad6571dae4489955
lthompson <lthompson@infomedia.com.au>
2017-08-05 20:50:01 +1000

    docs: Update the example code for date / time picker

--- sample/src/app/components/datepicker.ts
+++ sample/src/app/components/datepicker.ts
@@ -6,56 +6,96 @@ declare var Materialize: any;
 @Component({
     selector: "datePicker",
     template: `
+    <div class="row"><h4>Simple Binding</h4></div>
     <div class="row">
           <div class="col s6">
-            <label for="birthdate">Birthdate No Form {{birthdate}}</label>
-            <input id="birthdate" name="birthdate" [(ngModel)]="birthdate"
+            <label for="birthdate">Birthdate {{birthDate}}</label>
+            <input id="birthdate" name="birthdate" [(ngModel)]="birthDate"
                    materialize="pickadate"
-                   [materializeActions]="actions"
+                   [materializeActions]="birthDateActions"
                    [materializeParams]="[{format: 'dd/mm/yyyy'}]"
                    type="text" />
           </div>
-          <div class="col s4">
-             <label for="birthtime">Birthtime No Form</label>
-             <input id="birthtime"
-                    materialize="pickatime" [materializeParams]="[{default: '00:00', twelvehour: false}]"
+          <div class="col s6">
+             <label for="birthtime">Birthtime {{birthTime}}</label>
+             <input id="birthtime" name="birthtime" [(ngModel)]="birthTime"
+                    materialize="pickatime" 
+                    [materializeActions]="birthTimeActions"
+                    [materializeParams]="[{twelvehour: false}]"
                     placeholder="00:00" class="validate" type="text">
           </div>
+          
     </div>
+    <div class="row">
+          <div class="col s6">
+            <a class="waves-effect waves-light btn" (click)="openDatePicker()">Open Date Picker</a>
+          </div>
+            
+          <div class="col s3">
+           <a class="waves-effect waves-light btn" (click)="setTime('01:15')">Set Time to 01:15</a>
+          </div>
+          <div class="col s2">
+           <a class="waves-effect waves-light btn" (click)="openTimePicker()">Open Time Picker</a>
+          </div>
+    </div>
+    
+    <div class="row"></div>
+    <div class="row"></div>
+    
+    <div class="row"><h4>Form Binding</h4></div>
+    
     <form [formGroup]="form" materialize class="col s12">
         <div class="row">
-           <div class="col s6">
-            <label for="birthdate">Birthdate 06/07/2017</label>
-            <input id="birthdate" name="birthdate" formControlName="from"
+           <div class="col s4">
+          
+            <label for="birthdate2">Birthdate {{form.value.fromDate}}</label>
+            <input id="birthdate2" name="birthdate2" formControlName="fromDate"
                    materialize="pickadate"
-                   [materializeActions]="actions"
+                   [materializeParams]="[{format: 'dd/mm/yyyy'}]"
+                   type="text" />
+          </div>
+          </div>
+          <div class="row">
+           <div class="col s4">
+            <label for="birthtime2">Birthtime {{form.value.fromTime}}</label>
+            <input id="birthtime2" name="birthtime2" formControlName="fromTime"
+                   materialize="pickatime"
                    [materializeParams]="[{format: 'dd/mm/yyyy'}]"
                    type="text" />
           </div>
         </div>
     </form>
-    <br/><br/>
-    <a class="waves-effect waves-light btn" (click)="openAndClose()">Open and Close</a>
         `
 })
 export class DatePicker {
-    birthdate;
+    birthDate:string;
+    birthTime:string;
 
-    actions = new EventEmitter<string|MaterializeAction>();
+    birthDateActions = new EventEmitter<string|MaterializeAction>();
+    birthTimeActions = new EventEmitter<string|MaterializeAction>();
     form: FormGroup;
 
 
     constructor(private fb: FormBuilder) {
-        this.birthdate = new Date('03/12/2017');
+        this.birthDate = "03/12/2017";
+        this.birthTime = "12:36";
         this.form = this.fb.group({
-            'from': new FormControl('06/07/2017'),
+            'fromDate': new FormControl('06/07/2017'),
+            'fromTime': new FormControl('08:30')
         });
     }
 
-    openAndClose() {
-        this.actions.emit({action: "pickadate", params: ["open"]});
-        window.setTimeout(()=> {
-            this.actions.emit({action: "pickadate", params: ["close"]});
-        }, 1000);
+    openDatePicker() {
+        //actions are open or close
+        this.birthDateActions.emit({action: "pickadate", params: ["open"]});
+    }
+
+    setTime(time) {
+        this.birthTime = time;
+    }
+
+    openTimePicker() {
+        //actions are show or hide
+        this.birthTimeActions.emit({action: "pickatime", params: ["show"]});
     }
 }

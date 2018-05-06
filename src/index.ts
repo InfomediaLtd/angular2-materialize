b31320ad213d3bfc4337d360a4be5113baf89deb
Joshua Chan Mun Wei <joscmw95@hotmail.com>
2018-05-07 01:43:34 +0800

    added isPlatformBrowser check and proper window object checking

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-export {MaterializeDirective,MaterializeAction} from "./materialize-directive";
-export {MaterializeModule} from "./materialize-module";
+export { MaterializeDirective, MaterializeAction } from "./materialize-directive";
+export { MaterializeModule } from "./materialize-module";
 
-if (!("Materialize" in window)) {
-  throw new Error("Couldn't find Materialize object on window. It is created by the materialize-css library. Please import materialize-css before importing angular2-materialize.");
+declare var Waves: any;
+declare var Materialize: any;
+
+if ("undefined" != typeof window) {
+  if (!("Materialize" in window)) {
+    throw new Error("Couldn't find Materialize object on window. It is created by the materialize-css library. Please import materialize-css before importing angular2-materialize.");
+  }
+  if (!("Waves" in window)) {
+    throw new Error("Couldn't find Waves object on window. It is supposed to be created by the materialize-css library. Please import materialize-css before importing angular2-materialize.");
+  }
+
+  Waves.displayEffect();
+  // polyfill remove any elem in DOM - https://github.com/InfomediaLtd/angular2-materialize/issues/377 (IE)
+  if (!Element.prototype.remove) {
+    Element.prototype.remove = function remove() {
+      if (this.parentNode) {
+        this.parentNode.removeChild(this);
+      }
+    };
+  }
 }
-if (!("Waves" in window)) {
-  throw new Error("Couldn't find Waves object on window. It is supposed to be created by the materialize-css library. Please import materialize-css before importing angular2-materialize.");
-}
-
-declare var Waves:any;
-Waves.displayEffect();
-
-declare var Materialize:any;
 
 export function toast(...args) {
   Materialize.toast(...args);
-}
-
-// polyfill remove any elem in DOM - https://github.com/InfomediaLtd/angular2-materialize/issues/377 (IE)
-if (!Element.prototype.remove) {
-  Element.prototype.remove = function remove() {
-    if (this.parentNode) {
-      this.parentNode.removeChild(this);
-    }
-  };
 }

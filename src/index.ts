7af4bedfb53ace1594bdaf434a42589e7950d58c
Metin Kocaman <metin.kocaman@gmx.de>
2018-06-22 09:15:51 +0200

    Moved the files in the correct  folder
    Moved the files in the correct  folder

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export {MaterializeDirective,MaterializeAction} from "./materialize-directive";
 export {MaterializeModule} from "./materialize-module";
 
-if (!("Materialize" in window)) {
+if (!("M" in window)) {
   throw new Error("Couldn't find Materialize object on window. It is created by the materialize-css library. Please import materialize-css before importing angular2-materialize.");
 }
 if (!("Waves" in window)) {
@@ -11,10 +11,10 @@ if (!("Waves" in window)) {
 declare var Waves:any;
 Waves.displayEffect();
 
-declare var Materialize:any;
+declare var M:any;
 
 export function toast(...args) {
-  Materialize.toast(...args);
+  M.toast(...args);
 }
 
 // polyfill remove any elem in DOM - https://github.com/InfomediaLtd/angular2-materialize/issues/377 (IE)
